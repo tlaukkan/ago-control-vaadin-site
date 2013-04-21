aaad22bcff50d9d5ec7b1b3d5e7173a3a2eb3683
tlaukkan <tommi.s.e.laukkanen@gmail.com>
2013-04-21 09:11:27 +0300

    Added support for speech synthesis using flite shell command.

--- ago-control-server-agent/src/main/java/org/agocontrol/agent/ShellCommandListener.java
+++ ago-control-server-agent/src/main/java/org/agocontrol/agent/ShellCommandListener.java
@@ -19,7 +19,9 @@ import org.apache.log4j.Logger;
 
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
+import java.util.HashSet;
 import java.util.Map;
+import java.util.Set;
 
 /**
  * The shell command listener.
@@ -29,32 +31,71 @@ import java.util.Map;
 public class ShellCommandListener implements CommandListener {
     /** The logger. */
     private static final Logger LOGGER = Logger.getLogger(ShellCommandListener.class);
+    /** List of allowed characters in shell command parameters. */
+    private static final String ALLOWD_CHARACTERS_STRING =
+            "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!., ";
+    /** Lookup set for allowed characters. */
+    private static final Set<Character> ALLOWED_CHARACTERS = new HashSet<Character>();
+    static {
+        for (final Character character : ALLOWD_CHARACTERS_STRING.toCharArray()) {
+            ALLOWED_CHARACTERS.add(character);
+        }
+    }
 
     @Override
-    public final Map<String, Object> commandReceived(final Map<String, Object> command) {
-        LOGGER.debug("Processing command: " + command.toString());
+    public final Map<String, Object> commandReceived(final Map<String, Object> parameters) {
+        LOGGER.debug("Received command: " + parameters.toString());
 
-        if ("screenon".equals(command.get("command"))) {
+        final String command = (String) parameters.get("command");
+
+        if ("screenon".equals(command)) {
             executeShellCommand("/usr/bin/xset dpms force on");
         }
 
-        if ("screenoff".equals(command.get("command"))) {
+        if ("screenoff".equals(command)) {
             executeShellCommand("/usr/bin/xset dpms force off");
+        }
+
+        if ("say".equals(command)) {
+            final String message = sanitize((String) parameters.get("message"));
+            if (message.length() > 0) {
+                executeShellCommand("flite -t '" + message + "' -voice slt");
+            }
         }
 
         return null;
     }
 
     /**
+     * Sanitizes text to remove any characters which could be harmful when used as shell command parameters.
+     *
+     * @param text the text to sanize
+     * @return
+     */
+    private String sanitize(final String text) {
+        if (text == null) {
+            return "";
+        }
+        final StringBuilder stringBuilder = new StringBuilder();
+        for (final Character character : text.toCharArray()) {
+            if (ALLOWED_CHARACTERS.contains(character)) {
+                stringBuilder.append(character);
+            }
+        }
+        return stringBuilder.toString().trim();
+    }
+
+
+    /**
      * Executes requested shell command.
      *
      * @param cmd the shell command to execute
      */
-    private void executeShellCommand(String cmd) {
+    private void executeShellCommand(final String cmd) {
         LOGGER.debug("Executing: " + cmd);
         try {
             Runtime runtime = Runtime.getRuntime();
-            Process process = runtime.exec(cmd);
+            Process process = runtime.exec(new String[] {"/bin/sh", "-c", cmd});
             process.waitFor();
 
             String line;
